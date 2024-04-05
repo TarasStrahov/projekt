6df07bfccf8c371c643f2c939ee9faaa9f1762ed
TarasStrahov <taras.strahov@gmail.com>
2024-04-05 12:22:53 +0200

    prva verzia

---
+++ partials/header.php
@@ -0,0 +1,14 @@
+<header class="container main">
+            <nav class="main-nav">
+                <ul class="main-menu" id="main-menu">
+                    <li><a href="contacts.html">Kontakty</a></li>
+                    <li><a href="index.html">Domov</a></li>
+                    <li><a href="onas.html">O nás</a></li>
+                    <li><a href="qna.html">Q&A</a></li>
+                    <img src="img/porsche-crest.min.99bed7254e46aa46a8391f715158a5fc@1x.webp">
+                </ul>
+                <a class="hamburger" id="hamburger">
+                    <i class="fa fa-bars"></i>
+                    </a>
+                </a>
+    </header>
